30f44a5af1c8811aa1442004f36daf42b0f28e85
Rokas <rookas.rudzenskas@gmail.com>
2022-10-11 16:11:34 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import {FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import RestaurantItem from "./src/components/RestaurantItem";
 import Restaurants from './src/assets/data/restaurants.json'
+import { LogBox } from 'react-native';
+import HomeScreen from "./src/screens/HomeScreen";
+
+LogBox.ignoreLogs(['SplashScreen.show']);
 
 export default function App() {
     return (
         <View className="h-screen mt-10">
-            <FlatList data={Restaurants} showsVerticalScrollIndicator={false} renderItem={({item}) => <RestaurantItem restaurant={item} />} keyExtractor={(item) => item.id} />
+            {/*<HomeScreen />*/}
+
             <StatusBar style="auto" />
         </View>
     );
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet, FlatList} from 'react-native';
+import Restaurants from "../../assets/data/restaurants.json";
+import RestaurantItem from "../../components/RestaurantItem";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <FlatList data={Restaurants} showsVerticalScrollIndicator={false} renderItem={({item}) => <RestaurantItem restaurant={item} />} keyExtractor={(item) => item.id} />
+        </View>
+    );
+};
+
+export default HomeScreen;
