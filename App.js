bfebd0324fe38457acab872f5f795bb55a05a345
Rokas <rookas.rudzenskas@gmail.com>
2022-10-11 09:43:54 +0300

    Rokas

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
       <View className="flex-1 items-center justify-center bg-white">
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hello People!</Text>
         <StatusBar style="auto" />
       </View>
   );
