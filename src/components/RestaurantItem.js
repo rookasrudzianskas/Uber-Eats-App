71b43d0923f9d69ce7a1e4e92ccc0f5b3eb6a6f5
Rokas <rookas.rudzenskas@gmail.com>
2022-10-11 10:53:48 +0300

    Rokas

--- src/components/RestaurantItem.js
+++ src/components/RestaurantItem.js
@@ -9,13 +9,13 @@ const RestaurantCard = ({restaurant}) => {
                 <View className="space-y-1">
                     <Text className="text-gray-900 font-[600] text-[18px]">{restaurant?.name}</Text>
                     <View className="flex-row items-center">
-                        <Text className="text-gray-400 text-[15px]">1.4</Text>
-                        <Text className="text-gray-400 text-[15px]">•</Text>
-                        <Text className="text-gray-400 text-[15px]">20 - 30 min</Text>
+                        <Text className="text-gray-400 text-[15px]">{restaurant?.deliveryFee}</Text>
+                        <Text className="text-gray-400 text-[15px]"> • </Text>
+                        <Text className="text-gray-400 text-[15px]">{restaurant?.minDeliveryTime} - {restaurant?.maxDeliveryTime} min</Text>
                     </View>
                 </View>
                 <View className="bg-gray-200 rounded-full items-center justify-center h-8 w-8 m-2">
-                    <Text className="font-[600] text-gray-700">4.3</Text>
+                    <Text className="font-[600] text-gray-700">{restaurant?.rating}</Text>
                 </View>
             </View>
         </TouchableOpacity>
