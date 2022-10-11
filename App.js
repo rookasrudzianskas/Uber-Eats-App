6e20adae40ab67e0b25fb5a627b76fe44bcfc8c2
Rokas <rookas.rudzenskas@gmail.com>
2022-10-11 10:18:21 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import RestaurantItem from "./src/components/RestaurantItem";
+import Restaurants from './src/assets/data/restaurants.json'
 
 export default function App() {
     return (
