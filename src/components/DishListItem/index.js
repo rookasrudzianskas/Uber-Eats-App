7e507ac1110d28e0467bcf43c6efdf046e68dd33
Rokas <rookas.rudzenskas@gmail.com>
2022-10-12 10:37:48 +0300

    Rokas

--- src/components/DishListItem/index.js
+++ src/components/DishListItem/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 const DishListItem = ({dish}) => {
     return (
-        <View className="mx-4 space-y-2">
-            <Text className="text-[17px] font-[600] text-gray-900">
+        <TouchableOpacity activeOpacity={0.7} className="mx-4 space-y-2 border-b border-gray-300 py-5">
+            <Text className="text-[17px] tracking-wide font-[600] text-gray-900">
                 {dish?.name}
             </Text>
             <Text className="text-gray-600">{dish?.description}</Text>
             <Text className="text-[16px] text-gray-900 font-[6500]">${dish?.price}</Text>
-        </View>
+        </TouchableOpacity>
     );
 };
 
