e6fdb722ee1e28a72c166e6fe048333901d7ce21
Rokas <rookas.rudzenskas@gmail.com>
2022-10-12 14:03:04 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import {View} from 'react-native';
 import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from "./src/navigation";
 
 LogBox.ignoreLogs(['SplashScreen.show']);
 
@@ -9,11 +10,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <View className="h-screen">
-                {/*<HomeScreen />*/}
-                {/*<RestaurantDetails />*/}
-                {/*<DishDetailsScreen />*/}
-                {/*<Basket />*/}
-                {/*<OrdersScreen />*/}
+                <RootNavigator />
                 <StatusBar style="auto" />
             </View>
         </NavigationContainer>
