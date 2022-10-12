c5a2bd218b94af4b807e1520dcbee914814cc7a3
Rokas <rookas.rudzenskas@gmail.com>
2022-10-12 14:08:06 +0300

    Rokas

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,14 +3,18 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from "../screens/HomeScreen";
+import RestaurantDetails from "../screens/RestaurantDetails";
 
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator initialRouteName="Home">
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantDetails} options={{
+                headerShown: false
+            }} />
         </Stack.Navigator>
     );
 }
