fbf03b5970f4791437f9bacce91c8339cc96780a
Rokas <rookas.rudzenskas@gmail.com>
2022-10-12 13:56:54 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import {FlatList, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import Restaurants from './src/assets/data/restaurants.json'
+import {View} from 'react-native';
 import { LogBox } from 'react-native';
-import HomeScreen from "./src/screens/HomeScreen";
-import RestaurantDetails from "./src/screens/RestaurantDetails";
-import DishDetailsScreen from "./src/screens/DishDetailsScreen";
-import Basket from "./src/screens/Basket";
-import OrdersScreen from "./src/screens/OrdersScreen";
 import { NavigationContainer } from '@react-navigation/native';
 
 LogBox.ignoreLogs(['SplashScreen.show']);
