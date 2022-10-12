fb42dc1c772a41f125f6d2d617d767b34347cc82
Rokas <rookas.rudzenskas@gmail.com>
2022-10-12 11:24:36 +0300

    Rokas

--- src/screens/DishDetailsScreen/index.js
+++ src/screens/DishDetailsScreen/index.js
@@ -6,6 +6,17 @@ import Restaurants from '../../assets/data/restaurants.json';
 const DishDetailsScreen = () => {
     const dish = Restaurants[0]?.dishes[0];
     const [quantity, setQuantity] = useState(1);
+
+    const onPlus = () => {
+        // make it always positive number
+        setQuantity(Math.max(0, quantity + 1));
+    }
+
+    const onMinus = () => {
+        // make it always positive number
+        setQuantity(Math.max(0, quantity - 1));
+    }
+
     return (
         <View className="relative h-screen bg-gray-100">
             <View className="px-4">
@@ -20,11 +31,11 @@ const DishDetailsScreen = () => {
             <View className="border-b border-gray-300 my-6"/>
 
             <View className="flex-row items-center justify-center space-x-4 mt-3">
-                <TouchableOpacity activeOpacity={0.7}>
+                <TouchableOpacity onPress={onPlus} activeOpacity={0.7}>
                     <AntDesign name="pluscircleo" size={35} color="black" />
                 </TouchableOpacity>
                 <Text className="text-gray-900 text-[17px]">{quantity}</Text>
-                <TouchableOpacity activeOpacity={0.7}>
+                <TouchableOpacity onPress={onMinus} activeOpacity={0.7}>
                     <AntDesign name="minuscircleo" size={35} color="black" />
                 </TouchableOpacity>
             </View>
