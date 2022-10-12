66e12dfcd0190c5629e2e08957ad668e8dba8df2
Rokas <rookas.rudzenskas@gmail.com>
2022-10-12 13:06:12 +0300

    Rokas

--- src/screens/OrdersScreen/index.js
+++ src/screens/OrdersScreen/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import OrderListItem from "../../components/OrderListItem";
-
+import orders from '../../assets/data/orders.json';
 const OrdersScreen = () => {
     return (
         <View>
