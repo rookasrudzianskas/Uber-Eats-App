20fbf44632ca3498159eb2ec4659abffbf48b1f9
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 11:13:33 +0300

    Rokas

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, FlatList} from 'react-native';
 import Restaurants from "../../assets/data/restaurants.json";
 import RestaurantItem from "../../components/RestaurantItem";
 
 const HomeScreen = () => {
+    const [restaurants, setRestaurants] = useState(Restaurants);
     return (
         <View className="mx-4">
             <FlatList
                 keyExtractor={(item) => item.id}
-                data={Restaurants}
+                data={restaurants}
                 showsVerticalScrollIndicator={false}
                 renderItem={({item}) => <RestaurantItem restaurant={item} />}
             />
