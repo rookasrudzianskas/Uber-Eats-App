253725d0738f1effb81f36b05db3b60768986ae0
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 13:32:20 +0300

    Rokas

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -6,9 +6,12 @@ const AuthContext = createContext({});
 const AuthContextProvider = ({children}) => {
     const [authUser, setAuthUser] = useState(null);
     const [dbUser, setDbUser] = useState(null);
+
+
     return (
         <AuthContext.Provider value={{
-
+            authUser,
+            dbUser,
         }}>
             {children}
         </AuthContext.Provider>
