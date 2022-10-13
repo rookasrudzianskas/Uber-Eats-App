65a9fa56099f5ac0284e28a502d4398ecaded8e2
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 13:31:47 +0300

    Rokas git push origin master

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,9 +1,11 @@
 // create context
-import {createContext} from "react";
+import {createContext, useState} from "react";
 
 const AuthContext = createContext({});
 
 const AuthContextProvider = ({children}) => {
+    const [authUser, setAuthUser] = useState(null);
+    const [dbUser, setDbUser] = useState(null);
     return (
         <AuthContext.Provider value={{
 
