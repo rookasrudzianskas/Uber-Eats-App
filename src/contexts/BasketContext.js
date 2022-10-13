ff0d3fc24482cab47facffdad5990c992d7fef03
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 15:17:35 +0300

    Rokas

--- src/contexts/BasketContext.js
+++ src/contexts/BasketContext.js
@@ -1,11 +1,16 @@
 // create Basket context
-import {createContext, useContext} from 'react';
+import {createContext, useContext, useState} from 'react';
+import {useAuthContext} from "./AuthContext";
 
 const BasketContext = createContext();
 
 const BasketContextProvider = ({ children }) => {
+    const {sub} = useAuthContext();
+    const [restaurant, setRestaurant] = useState(null);
+    const [basket, setBasket] = useState(null);
     const addDishToBasket = (dish, quantity) => {
         // console.log('addDishToBasket', dish, quantity);
+        // get the existing basket from local storage or  create a new one
         // create a BasketDishItem and add to cloud
 
     }
