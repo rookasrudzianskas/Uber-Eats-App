e8886eb89d429b6fb0ca04693d452fe6c0a36520
Rokas <rookas.rudzenskas@gmail.com>
2022-10-13 15:23:21 +0300

    Rokas

--- src/contexts/BasketContext.js
+++ src/contexts/BasketContext.js
@@ -1,13 +1,20 @@
 // create Basket context
-import {createContext, useContext, useState} from 'react';
+import {createContext, useContext, useEffect, useState} from 'react';
 import {useAuthContext} from "./AuthContext";
+import {DataStore} from "aws-amplify";
+import {Basket} from "../models";
 
 const BasketContext = createContext();
 
 const BasketContextProvider = ({ children }) => {
-    const {sub} = useAuthContext();
+    const {dbUser} = useAuthContext();
     const [restaurant, setRestaurant] = useState(null);
     const [basket, setBasket] = useState(null);
+
+    useEffect(() => {
+        DataStore.query(Basket, b => b.restaurantID("eq", restaurant.id).userID("eq", dbUser.id)).then(baskets => setBasket(baskets[0]));
+    }, [dbUser, restaurant]);
+
     const addDishToBasket = (dish, quantity) => {
         // console.log('addDishToBasket', dish, quantity);
         // get the existing basket from local storage or  create a new one
